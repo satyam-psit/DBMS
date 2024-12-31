480269887c327a4e4d5335f302fca32eef14faee
Satyam Trivedi <satyamtrivedi0104315@gmail.com>
2024-12-31 10:38:45 +0530

    Update 27dec-question.sql

--- 27dec-question.sql
+++ 27dec-question.sql
@@ -108,5 +108,54 @@ WHERE
     orderdetails.productid = 2;
 
 
+CREATE TABLE sales1 (
+    saleid INT PRIMARY KEY,
+    productid INT,
+    customerid INT,
+    saledate DATE,
+    amount DECIMAL(10, 2)
+);
+
+INSERT INTO sales1 (saleid, productid, customerid, saledate, amount)
+VALUES (1, 101, 201, TO_DATE('2023-01-01', 'YYYY-MM-DD'), 500);
+INSERT INTO sales1 (saleid, productid, customerid, saledate, amount)
+values(2, 102, 202, TO_DATE('2023-01-02', 'YYYY-MM-DD'), 300);
+INSERT INTO sales1 (saleid, productid, customerid, saledate, amount)
+values(3, 101, 201, TO_DATE('2023-01-03', 'YYYY-MM-DD'), 700);
+INSERT INTO sales1 (saleid, productid, customerid, saledate, amount)
+values(4, 103, 203, TO_DATE('2023-01-04', 'YYYY-MM-DD'), 200);
+INSERT INTO sales1 (saleid, productid, customerid, saledate, amount)
+values(5, 102, 202, TO_DATE('2023-01-05', 'YYYY-MM-DD'), 400);
+
+-- Verify the table content
+SELECT * FROM sales1;
+
+CREATE TABLE product1(
+    productid INT PRIMARY KEY,
+    productname VARCHAR2(100)
+);
+
+INSERT INTO product1 (productid, productname)
+VALUES(101, 'Laptop');
+INSERT INTO product1 (productid, productname)
+values(102, 'phone');
+INSERT INTO product1 (productid, productname)
+values(103, 'Tablet');
+
+-- Verify the table content
+SELECT * FROM product1;
+
+SELECT p.productname, SUM(s.amount) AS total_sales
+FROM sales1 s
+JOIN product1 p ON s.productid = p.productid
+GROUP BY p.productname
+HAVING SUM(s.amount) > 600;
+
+SELECT p.productname, COUNT(DISTINCT s.customerid) AS distinct_customers
+FROM sales1 s
+JOIN product1 p ON s.productid = p.productid
+GROUP BY p.productname;
+
+
 
 
